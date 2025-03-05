cf6385882f24aedf3126e361875841081fae5fd0
SivenM <aztek8877@gmail.com>
2025-03-05 10:53:38 +0300

    qwd

--- file2.py
+++ file2.py
@@ -2,11 +2,12 @@ b = "aaaaaaaaaaaaaasdfaffd"
 b = "aaaaaaaaaaaaaasdfaffd"
 b = "aaaaaaaaaaaaaasdfaffd"
 b = "aaaaaaaaaaaaaasdfaffd"
-b = "aaaaaaaaaaaaaasdfaffd"
-b = "aaaaaaaaaaaaaasdfaffd"
-b = "aaaaaaaaaaaaaasdfaffd"
-b = "aaaaaaaaaaaaaasdfaffd"
-b = "aaaaaaaaaaaaaasdfaffd"
+def create_delta_day(self, commit_date, author_last_date):
+        if author_last_date == None:
+            delta_day = 0.
+        else:
+            delta_day = self._create_delta(commit_date, author_last_date)
+        return delta_day
 'asdsadasdasdasdad'
 'asdsadasdasdasdad'
 'asdsadasdasdasdad'
--- mega.py
+++ mega.py
@@ -163,9 +163,23 @@ def create_delta_day(self, commit_date, author_last_date):
 'petgnw[gunw[]]'
 'petgnw[gunw[]]'
 'petgnw[gunw[]]'
-def create_delta_day(self, commit_date, author_last_date):
-        if author_last_date == None:
-            delta_day = 0.
-        else:
-            delta_day = self._create_delta(commit_date, author_last_date)
-        return delta_day
+'petgnw[gunw[]]'
+'petgnw[gunw[]]'
+'petgnw[gunw[]]'
+'petgnw[gunw[]]'
+'petgnw[gunw[]]'
+'petgnw[gunw[]]'
+'petgnw[gunw[]]'
+'petgnw[gunw[]]''petgnw[gunw[]]'
+'petgnw[gunw[]]'
+'petgnw[gunw[]]'
+'petgnw[gunw[]]''petgnw[gunw[]]'
+'petgnw[gunw[]]'
+'petgnw[gunw[]]'
+'petgnw[gunw[]]''petgnw[gunw[]]'
+'petgnw[gunw[]]'
+'petgnw[gunw[]]'
+'petgnw[gunw[]]''petgnw[gunw[]]'
+'petgnw[gunw[]]'
+'petgnw[gunw[]]'
+'petgnw[gunw[]]'
