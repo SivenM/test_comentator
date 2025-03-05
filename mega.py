dfc9930f6c26c72abf947de92eb0ce026b1e961b
SivenM <aztek8877@gmail.com>
2025-03-05 11:17:06 +0300

    ascac

--- mega.py
+++ mega.py
@@ -187,12 +187,16 @@ def create_delta_day(self, commit_date, author_last_date):
 def create_delta_day(self, commit_date, author_last_date):
         if author_last_date == None:
             delta_day = 0.
-        else:
-            delta_day = self._create_delta(commit_date, author_last_date)
-        return delta_day
+'petgnw[gunw[]]'
+'petgnw[gunw[]]'
+'petgnw[gunw[]]'
 def create_delta_day(self, commit_date, author_last_date):
         if author_last_date == None:
             delta_day = 0.
         else:
             delta_day = self._create_delta(commit_date, author_last_date)
         return delta_day
+
+def create_delta_day(self, commit_date, author_last_date):
+    if author_last_date == None:
+        delta_day = 0
