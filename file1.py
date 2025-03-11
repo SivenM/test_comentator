d339bc7405601d34074079bcdad23f6828ae961b
SivenM <aztek8877@gmail.com>
2025-03-11 17:07:03 +0300

    asc

--- file1.py
+++ file1.py
@@ -74,37 +74,17 @@ a = mul(a,a)
 a = mul(a,a)
 a = mul(a,a)
 a = mul(a,a)
+def get_loss(clf, X, y):
+    loss = []
+    # Используйте метод staged_decision_function для предсказания качества
+    # на обучающей и тестовой выборке на каждой итерации.
+    for y_pred in clf.staged_decision_function(X):
+        # Вычислите и постройте график значений log-loss
 a = mul(a,a)
 a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
-a = mul(a,a)
+def get_loss(clf, X, y):
+    loss = []
+    # Используйте метод staged_decision_function для предсказания качества
+    # на обучающей и тестовой выборке на каждой итерации.
+    for y_pred in clf.staged_decision_function(X):
+        # Вычислите и постройте график значений log-loss
